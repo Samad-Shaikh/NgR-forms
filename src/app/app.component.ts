4072acd695fa9f5d4c79c9a2102a0a04b1de3a2c
Binyamin <samadchaz@hotmail.com>
2022-11-02 21:06:43 +0100

    add project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngr-forms-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'ngr-forms';
+}
